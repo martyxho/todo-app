820d39dec654edd4295ed337fff8e051e37353fd
Marty <maruri2424@gmail.com>
2022-04-17 22:43:17 -0700

    styled css and added form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import './style.css';
 
-function toDoFactory(title, description, dueDate, priority) {
+const autorun = (() => {
+  const taskBtn = document.getElementById('taskBtn');
+  taskBtn.onclick = openTaskForm;
+})();
+
+function task(title, description, dueDate, priority) {
   return {title, description, dueDate, priority}
 }
 
+function project(name) {
+  const arr = [];
+  function addTask(task) {
+    arr.push(task);
+  }
+  function getArr() {
+    return arr;
+  }
+  return {addTask, getArr};
+}
+
+function openTaskForm() {
+  const tint = document.querySelector('.tint');
+  tint.classList.toggle('dim');
+  const form = document.querySelector('.form');
+  form.classList.toggle('hideform');
+}
